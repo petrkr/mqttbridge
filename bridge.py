e9c9a046969a0500287543772053ead06de97fcb
Petr Kracík <petrkr@petrkr.net>
2024-10-19 18:00:42 +0200

    Set up logger

--- bridge.py
+++ bridge.py
@@ -1,7 +1,11 @@
 import paho.mqtt.client as mqtt
 import yaml
 import ssl
+import logging
 
+version = "0.0.1"
+
+logger = logging.getLogger("bridge")
 
 def load_config(config_file):
     with open(config_file, 'r') as file:
@@ -46,18 +50,24 @@ def connect_mqtt(broker_config):
     client = mqtt.Client(client_id)
 
     if username and password:
+        logger.debug(f"Setting up user/pass for {host}")
         client.username_pw_set(username, password)
 
     if tls_enabled:
+        logger.debug(f"Activating TLS for {host}")
         client.tls_set_context(ssl.create_default_context())
     
+    logger.info(f"Connecting to {host}:{port} with clientId: {client_id}")
     client.connect(host, port)
 
     return client
 
 
-def main():
-    config = load_config('config.yaml')
+def main(cfg_file="config/config.yaml"):
+
+    logger.info(f"Starting MQTT Bridge v{version}")
+    logger.info(f"Loading config file {cfg_file}")
+    config = load_config(cfg_file)
 
     client_src = connect_mqtt(config['broker_src'])
     client_dst = connect_mqtt(config['broker_dst'])
@@ -67,24 +77,35 @@ def main():
 
     client_src.on_message = on_src_message
 
+    logger.info("Subscribe source mqtt topics")
     for src_topic in [mapping["src_topic"] for mapping in config['mappings']]:
         if "+" in src_topic:
-            print(f"Plus wildcards not supported yet, skipping {src_topic}")
+            logger.warn(f"Plus wildcards not supported yet, skipping {src_topic}")
             continue
 
+        logger.debug(f"Adding topic {src_topic}")
         client_src.subscribe(src_topic)
 
 
+    logger.info("Starting source client loop")
     client_src.loop_start()
+
+    logger.info("Starting destination client loop")
     client_dst.loop_start()
 
     try:
         while True:
             pass
     except KeyboardInterrupt:
-        print("Exiting...")
+        logger.info("Exiting...")
+
+        logger.info("Stopping source client loop")
         client_src.loop_stop()
+
+        logger.info("Stopping destination client loop")
         client_dst.loop_stop()
 
+
 if __name__ == "__main__":
+    logging.basicConfig(format="{asctime} - {levelname} - {message}", style="{", datefmt="%Y-%m-%d %H:%M:%S %z", level=logging.DEBUG)
     main()
